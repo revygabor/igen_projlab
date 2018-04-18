27630e203bdab7a3d3bcd58845eeada5e952ca88
daniel <szekeresdani@gmail.com>
2018-04-18 17:02:34 +0200

    field from input done except for effects

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import javax.sound.midi.Soundbank;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,6 +16,8 @@ public class Main {
     }
 
     private static void newGame() {
+        _lonelyHiddenHoleCache.clear();
+        _dummyHiddenHoleCache.clear();
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         String path = "";
         while(true) {
@@ -30,7 +31,6 @@ public class Main {
         }
     }
 
-    List<List<Field>> fieldTable = new ArrayList<>();
 
     private static void readInputFile(String path) throws IOException {
         BufferedReader file = new BufferedReader(new FileReader(path));
@@ -44,7 +44,7 @@ public class Main {
         }
         while ((line = file.readLine()) != null) {
             split = line.split(" ");
-            //TODO: finish
+            //TODO: finish, create fields and set neighbors
         }
     }
 
@@ -68,12 +68,64 @@ public class Main {
         throw new NotImplementedException();
     }
 
-    private Field fieldFromInput(String desc) {
-        Pattern p = Pattern.compile("^(?<fieldtype>H|(H\\d+([H_]))|(S\\d+([NF]))|P|O|_)(?<effect>[HON])(?<contained>(W\\d+)|B)?");
+
+    private static Map<Integer, HiddenHole> _lonelyHiddenHoleCache = new HashMap<>();
+    private static Map<Integer, HiddenHole> _dummyHiddenHoleCache = new HashMap<>();
+    private static Field fieldFromInput(String desc) {
+        Pattern p = Pattern.compile("^(?<fieldtype>H|(H(?<hid>\\d+)([H_]))|(S(?<sid>\\d+)([NF]))|P|O|_)(?<effect>[HON])(?<contained>(W(?<wid>\\d+))|B)?");
         Matcher matcher = p.matcher(desc);
         if(matcher.matches()) {
             String fieldtype = matcher.group("fieldtype");
+            String effect = matcher.group("effect");
+            String contained = matcher.group("contained");
+            Field f;
+            if(fieldtype == null) {
+                System.out.println("Wrong field type");
+                return null;
+            }
+            if(effect == null) {
+                System.out.println("Wrong effect");
+                return null;
+            }
+            if(fieldtype.equals("H"))
+                f = new Hole();
+            else if(fieldtype.equals("P"))
+                f = new BoxPlace();
+            else if(fieldtype.equals("O"))
+                f = new Obstacle();
+            else if(fieldtype.equals("_"))
+                f = new Floor();
+            else if(fieldtype.charAt(0) == 'H') {
+                int id = Integer.parseInt(matcher.group("hid"));
+                if(_dummyHiddenHoleCache.containsKey(id)) {
+                    f = _dummyHiddenHoleCache.get(id);
+                } else {
+                    f = new HiddenHole();
+                    _lonelyHiddenHoleCache.put(id, (HiddenHole) f);
+                }
+            } else {
+                int id = Integer.parseInt(matcher.group("sid"));
+                if(_lonelyHiddenHoleCache.containsKey(id))
+                    f = new Switch(_lonelyHiddenHoleCache.get(id));
+                else {
+                    HiddenHole dummyHH = new HiddenHole();
+                    f = new Switch(dummyHH);
+                    _dummyHiddenHoleCache.put(id, dummyHH);
+                }
+            }
 
+            //TODO: effects
+
+            if(contained != null) {
+                if(contained.equals("B"))
+                    f.accept(new Box(), null);
+                else {
+                    int id = Integer.parseInt(matcher.group("wid"));
+                    f.accept(workers[id], null);
+                }
+            }
+
+            return f;
         }
         return null;
     }
